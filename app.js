9673d37923a6b5c6a061f233b376894c449b9417
Ian James <ianor182@gmail.com>
2019-02-23 20:50:13 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser);
+
+const port = process.env.port;
+const slackURL = process.env.slackURL;
+const secret = process.env.secret;
+
+app.post('/:secret', (req, res) => {
+  if (req.params.secret !== secret) return;
+  const payload = req.body;
+  console.log(payload);
+
+  //transform to a proper slack payload and post to slackURL
+});
+
+app.listen(port, () => console.info(`Application running on port ${port}`));
