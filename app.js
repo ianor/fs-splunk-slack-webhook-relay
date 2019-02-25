2e770a527a61084e759cc977c6b134e6ba643f2a
Ian James <ianor182@gmail.com>
2019-02-25 09:24:59 -0700

    Slack post added

--- app.js
+++ app.js
@@ -9,11 +9,52 @@ const slackURL = process.env.slackURL;
 const secret = process.env.secret;
 
 app.post('/:secret', (req, res) => {
-  if (req.params.secret !== secret) return;
+  if (req.params.secret !== secret) {
+    console.warn('*** Wrong secret ***')
+    return;
+  }
   const payload = req.body;
   console.log(payload);
 
   //transform to a proper slack payload and post to slackURL
+  const username = 'Splunk';
+  const link = payload.results_link; //eslint-disable-line camelcase
+  const { daysSinceLastEvent } = payload.results;
+  const message = `<!channel> There has not been an ingest in *${daysSinceLastEvent}* days.
+  Something might be wrong.`;
+  let postData = {
+    username,
+    text: message,
+    //icon_emoji: emoji, // eslint-disable-line camelcase
+    attachments: [
+      {
+        fallback: username,
+        color: '#FCB34B',
+        text: link
+      }
+    ]
+  };
+
+  fetch(slackURL, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: { 'Content-Type': 'application/json' }
+  }).then(res => {
+    if (!res.ok) console.log('*** Failed to post ***');
+  });
+
+  
+  /*
+  {
+    owner: 'ianor182',
+    sid: 'scheduler__ianor182_ZnMtaW5maW5pdHktZW5naW5lZXJpbmctbWV0cmljcw__Test_at_1550981700_30916_E2238C8D-27D7-436D-A927-1CC589B46408',
+    results_link: 'https://familysearch.splunkcloud.com/app/fs-infinity-engineering-metrics/@go?sid=scheduler__ianor182_ZnMtaW5maW5pdHktZW5naW5lZXJpbmctbWV0cmljcw__Test_at_1550981700_30916_E2238C8D-27D7-436D-A927-1CC589B46408',
+    app: 'fs-infinity-engineering-metrics',
+    result: { daysSinceLastEvent: '27' },
+    search_name: 'Test'
+  }
+  */
+
 });
 
 app.listen(port, () => console.info(`Application running on port ${port}`));
