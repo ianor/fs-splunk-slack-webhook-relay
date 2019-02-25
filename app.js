07523cf24404f1439e8acc1e2bff1a11cff646a8
Ian James <ianor182@gmail.com>
2019-02-25 15:49:20 -0700

    Formatting

--- app.js
+++ app.js
@@ -13,22 +13,36 @@ app.post('/:key', (req, res) => {
   const payload = req.body;
   //console.log(payload);
 
+  const username = 'Splunk';
+  const link = payload.results_link; //eslint-disable-line camelcase
+  const attachments = [{
+    fallback: username,
+    color: '#FCB34B',
+    text: link
+  }];
+
   let message = null;
   switch (req.params.key) {
     case ingestKey:
       const { folderCount, success, duration, artifactCount } = payload.result;
       const icon = success === 'true' ? ':white_check_mark:' : ':x:';
       const successful = success === 'true' ? 'successful' : 'failed'
-      message = `${icon} A *${successful}* ingest was logged by AMCS with the following information:
-      Duration: ${Math.round(10*parseInt(duration)/60/60)/10}h
-      Artifact Count: ${artifactCount}
-      Folder Count: ${folderCount}`;
+      message = `${icon} A *${successful}* ingest was logged by AMCS with the following information:`;
+      attachments.unshift({
+        fallback: username,
+        color: '#FCB34B',
+        text: `Duration: ${Math.round(10*parseInt(duration)/60/60)/10}h
+        Artifact Count: ${artifactCount}
+        Folder Count: ${folderCount}`
+      });
       break;
+
     case noIngestKey:
       const { daysSinceLastEvent } = payload.result;
       message = `:question: <!channel> There has not been an ingest in *${daysSinceLastEvent}* days (as reported by AMCS).
       Something might be wrong :grimacing:.`;
       break;
+
     default:
       console.warn('*** Key not recognized ***')
       res.status(501).end();
@@ -38,18 +52,11 @@ app.post('/:key', (req, res) => {
   
 
   //transform to a proper slack payload and post to slackURL
-  const username = 'Splunk';
-  const link = payload.results_link; //eslint-disable-line camelcase
+  
   let postData = {
     username,
     text: message,
-    attachments: [
-      {
-        fallback: username,
-        color: '#FCB34B',
-        text: link
-      }
-    ]
+    attachments
   };
 
   console.log(postData);
